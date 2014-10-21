36963a862c8da0ec8bf3728a50159401011bcc65
aliuzun <aliuzun@std.sehir.edu.tr>
2014-10-21 16:54:23 +0300

    collabration test on going

--- main/test1.py
+++ main/test1.py
@@ -3,5 +3,18 @@ __author__ = 'aliuzun'
 import unittest
 
 
-def firstFunction():
-    print  "Hello Github"
+
+class MyTest(unittest.TestCase):
+
+    def firstCheck(self):
+        self.assertEqual(2,2)
+
+    def new_func(self,data):
+        for i in data:
+            print i
+
+
+
+# if __name__= '__main__':
+#     unittest.main()
+
