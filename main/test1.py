fe2c19482e54288a390e2844f17abed137705bf8
abdullahoncu <abdullahon@gmail.com>
2014-10-21 17:06:59 +0300

    Merged and committing...

--- main/test1.py
+++ main/test1.py
@@ -10,10 +10,13 @@ class MyTest(unittest.TestCase):
         self.assertEqual(2,2)
 
     def new_func(self,data):
-        num=10
-        for i in data:
-            if num>i:
-                num==i
+        num=0
+        buffer = []
+        data = [2,3,45,67,123,123,45,6,7,9,10,8]
+        for i in range(sum(data)):
+            for j in range(i):
+                print j
+                buffer.append(j)
         return num
 
     def deneme(self,data,look):
